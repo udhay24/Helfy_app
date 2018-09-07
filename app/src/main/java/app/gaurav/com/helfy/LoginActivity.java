b6e54a804d1aeaaa7d31b8c9eb2d9cfa18d9e0e1
Gaurav <gauravrai568@gmail.com>
2018-09-07 13:41:56 +0530

    login screen addded

---
+++ app/src/main/java/app/gaurav/com/helfy/LoginActivity.java
@@ -0,0 +1,26 @@
+package app.gaurav.com.helfy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this , MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
