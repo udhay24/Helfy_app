2418eddb2a8be52cda2d32761dac037d0c3ddb66
udhay <udhayakumar241998@gmail.com>
2018-09-07 22:58:32 +0530

    Added order class

---
+++ app/src/main/java/app/gaurav/com/helfy/Model/Order.java
@@ -0,0 +1,76 @@
+package com.example.arvindbedi.helfy.Model;
+
+public class Order {
+
+    private String type, name, description, date, org, status, address;
+
+    public Order() {
+
+    }
+
+    public Order(String type, String name, String description, String date, String org, String status, String address) {
+        this.type = type;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.org = org;
+        this.status = status;
+        this.address = address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public void setOrg(String org) {
+        this.org = org;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
