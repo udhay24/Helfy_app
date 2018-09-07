1ba7861d401ed9c82cea4d5e12cde77a3e93ed47
Gaurav <gauravrai568@gmail.com>
2018-09-07 15:41:40 +0530

    activities added

--- app/src/main/java/app/gaurav/com/helfy/MainActivity.java
+++ app/src/main/java/app/gaurav/com/helfy/MainActivity.java
@@ -1,7 +1,11 @@
 package app.gaurav.com.helfy;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.icon_menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+                drawerLayout.openDrawer(Gravity.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/app/gaurav/com/helfy/SocialMediaActivity.java
@@ -0,0 +1,13 @@
+package app.gaurav.com.helfy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SocialMediaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_media);
+    }
+}
